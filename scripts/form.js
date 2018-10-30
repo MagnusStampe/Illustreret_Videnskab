a4c459477340a1b229bf60f1d3ede31f17615b38
Magnus stampe Nielsen <magnusstampenielsen@magnuss-mbp.home>
2018-10-30 19:21:10 +0100

    form started, progress bar almost done

---
+++ scripts/form.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const section = document.querySelector("#buy_form");
+const returnBtn = document.querySelector("#return");
+const confirmBtn = document.querySelector("#confirm");
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("init()");
+  returnBtn.addEventListener("click", goLeft);
+  confirmBtn.addEventListener("click", goRight);
+}
+
+function goLeft() {
+  console.log("goLeft()");
+
+  if (section.classList.value === "purchase") {
+    //FORM 1 to OUT
+    document.querySelector("#return_a").setAttribute("href", "index.html");
+  } else if (section.classList.value === "user_information") {
+    //FORM 2 to 1
+    console.log("User Information to Purchase");
+
+    //Class setup
+    section.classList.remove("user_information");
+    section.classList.add("purchase");
+
+    //Return button
+    returnBtn.textContent = "Tilbage til startside";
+  } else if (section.classList.value === "payment") {
+    //FORM 3 to 2
+    console.log("Payment to User Information");
+
+    //Class setup
+    section.classList.remove("payment");
+    section.classList.add("user_information");
+
+    //Confirm button
+    confirmBtn.textContent = "Næste";
+  }
+}
+
+function goRight() {
+  console.log("goRight()");
+
+  if (section.classList.value === "purchase") {
+    //FORM 1 to 2
+    console.log("Purchase to User Information");
+
+    //Class setup
+    section.classList.remove("purchase");
+    section.classList.add("user_information");
+
+    //Return button
+    returnBtn.textContent = "Tilbage";
+    document.querySelector("#return_a").setAttribute("href", "#buy_form");
+  } else if (section.classList.value === "user_information") {
+    //FORM 2 to 3
+    console.log("User Information to Payment");
+
+    //Class setup
+    section.classList.remove("user_information");
+    section.classList.add("payment");
+
+    //Confirm button
+    confirmBtn.textContent = "Gennemfør betaling";
+  } else if (section.classList.value === "payment") {
+    //FORM 3 to 4
+    console.log("Payment to Checkout");
+
+    //Class setup
+    section.classList.remove("payment");
+    section.classList.add("checkout");
+  }
+}
